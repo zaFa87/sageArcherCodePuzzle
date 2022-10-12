2d71a0c03ca72cee5f35390b21df5d5605ca4dd4
fabioos6 <fabio@onlinesucces.nl>
2022-10-12 15:42:14 +0200

    added global variable on tests

--- test_assignment.py
+++ test_assignment.py
@@ -1,22 +1,21 @@
 import unittest
-
-from main import convert_input_file_to_list, map_number_list, get_sum_metadata, get_number_of_nodes
+import main
+from main import convert_input_file_to_list, generate_node_list, get_sum_metadata
 
 
 class AssignmentTestCases(unittest.TestCase):
-    def test_correct_file(self):
+
+    def test_generate_list_of_nodes_metadata(self):
+        main.child_node_values = []
         test = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
-        self.assertEqual(([], [[10, 11, 12], [99], [2], [1, 1, 2]]), map_number_list(test))
+        generate_node_list(test)
+        self.assertEqual(([[10, 11, 12], [99], [2], [1, 1, 2]]), main.child_node_values)
 
     def test_metadata_sum(self):
-        test = (0, [[10, 11, 12], [99], [2], [1, 1, 2]])
-        self.assertEqual(138, get_sum_metadata(test))
+        main.child_node_values = [[10, 11, 12], [99], [2], [1, 1, 2]]
+        self.assertEqual(138, get_sum_metadata())
 
-    def test_number_of_nodes_in_file(self):
-        test = (0, [[10, 11, 12], [99], [2], [1, 1, 2]])
-        self.assertEqual(4, get_number_of_nodes(test))
-
-    def test_conversion(self):
+    def test_conversion_input_file_to_list(self):
         test = '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'
         self.assertEqual([2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2], convert_input_file_to_list(test))
 
@@ -24,11 +23,11 @@ class AssignmentTestCases(unittest.TestCase):
         test = '0 1 1'
         self.assertEqual([0, 1, 1], convert_input_file_to_list(test))
 
-    def test_file_too_short(self):
+    def test_conversion_file_too_short(self):
         test = '1'
         self.assertRaises(SystemExit, convert_input_file_to_list, test)
 
-    def test_file_with_letters(self):
+    def test_conversion_file_with_letters(self):
         test = 'r h 4'
         self.assertRaises(SystemExit, convert_input_file_to_list, test)
 
